0e18bd0eeff3f5cc5e993f9481ac4e56a941dc08
Massimiliano <massimilian.sampaolo@studenti.unicam.it>
2017-08-19 10:34:03 +0200

    Client: avanzamento implementazione servizio, implementate tutte le chiamate per la gestione dei contatti.

--- src/WebClient/src/app/services/iCamBankService.ts
+++ src/WebClient/src/app/services/iCamBankService.ts
@@ -3,14 +3,22 @@ import { IContact } from '../shared/models/contact';
 
 
 export interface ICamBankService {
-  authorize(): Promise<IAuthResponse>;
+  // Metodi di autenticazione
+  authorize(username, password): Promise<IAuthResponse>;
 
+  // Metodi per gestire la rubrica dell'utente loggato
   contacts(): Promise<IContact[]>;
+  addContact(contact): Promise<IContact>;
+  updateContact(iban, contact): Promise<IContact>;
+  deleteContact(iban): Promise<IContact>;
 }
 
 
 export abstract class CamBankService implements ICamBankService {
-  abstract authorize(): Promise<IAuthResponse>;
+  abstract authorize(username, password): Promise<IAuthResponse>;
 
   abstract contacts(): Promise<IContact[]>;
+  abstract addContact(contact): Promise<IContact>;
+  abstract updateContact(iban, contact): Promise<IContact>;
+  abstract deleteContact(iban): Promise<IContact>;
 }
