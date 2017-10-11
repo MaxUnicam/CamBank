82d7cfa07bbadd75d4b6e9b97b659d1caf7f74da
Massimiliano <massimilian.sampaolo@studenti.unicam.it>
2017-10-12 01:23:49 +0200

    Corretti errori

--- src/API/config.js
+++ src/API/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'superSecret',
-    'mongoConnectionString': 'process.env.MONGODB_URI'
+    'mongoConnectionString': 'mongodb://database_admin:Test_01@ds061158.mlab.com:61158/heroku_kgd1zlc3'
     // 'mongoConnectionString': 'mongodb://localhost/cambank'
 }
